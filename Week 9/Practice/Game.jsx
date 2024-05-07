67570733b4225b7bd7328b0bcbeb57f8ce4d14c5
Lachhman Das <101698396+lachhmandas377@users.noreply.github.com>
2024-05-07 17:05:14 +0500

    Add files via upload

---
+++ Week 9/Practice/Game.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Game() {
+  return (
+    <>
+      <div>
+        <button onClick={() => {addUser++
+        }
+
+        }>Add User</button>
+
+
+      </div>
+      <div className='addUser'>
+        <span>user </span>
+
+        <button>State 1</button>
+        <button>State 2</button>
+        <button>State 3</button>
+        <button>State 4</button>
+
+      </div>
+    </>
+  )
+}
